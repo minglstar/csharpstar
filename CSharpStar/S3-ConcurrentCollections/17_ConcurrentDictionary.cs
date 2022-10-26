692ec385c41851ccfd6d8f5171c6d7c1f4154934
minglstar <dreamingliu1101@gmail.com>
2022-10-26 11:18:23 +1100

    Add S3 files

---
+++ CSharpStar/S3-ConcurrentCollections/17_ConcurrentDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpStar
+{
+    public class ConcurrentDictionaryDemo
+    {
+        private static ConcurrentDictionary<String, String> capitals = new ConcurrentDictionary<string, string>();
+
+        public static void AddParis()
+        {
+            bool success = capitals.TryAdd("France", "Paris");
+
+            string who = Task.CurrentId.HasValue ? ("Task " + Task.CurrentId) : "Main thread";
+            Console.WriteLine($"{who} {(success ? "added" : "did not add")} the element.");
+        }
+
+        //public static void Main(string[] args)
+        //{
+       
+        //    Task.Factory.StartNew(AddParis).Wait();
+        //    AddParis();
+
+        //    //capitals["Russia"] = "Leningrad";
+        //    var s = capitals.AddOrUpdate("Russia", "Moscow", (k, old) => old + " --> Moscow");
+        //    Console.WriteLine("The capital of Russia is " + capitals["Russia"]);
+
+        //    capitals["Sweden"] = "Uppsala";
+        //    var capOfNorway = capitals.GetOrAdd("Sweden", "Stockholm");
+        //    Console.WriteLine($"The capital of Sweden is {capOfNorway}.");
+
+        //    // removal
+        //    const string toRemove = "Russia"; // make a mistake here
+        //    string removed;
+        //    var didRemove = capitals.TryRemove(toRemove, out removed);
+        //    if (didRemove)
+        //    {
+        //        Console.WriteLine($"We just removed {removed}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"Failed to remove capital of {toRemove}");
+        //    }
+
+        //    // some operations are slow, e.g.,
+        //    Console.WriteLine($"The ");
+
+        //    foreach (var kv in capitals)
+        //    {
+        //        Console.WriteLine($" - {kv.Value} is the capital of {kv.Key}");
+        //    }
+
+        //    Console.ReadKey();
+
+        //}
+    }
+}
+
---
+++ CSharpStar/S3-ConcurrentCollections/20_ConcurrentBag.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpStar
+{
+    internal static class ConcurrentBag
+    {
+        //static void Main()
+        //{
+        //    // stack Last in First out (LIFO)
+        //    // queue First in First out (FIFO)
+        //    // concurrent bag provides NO ordering guarantees
+
+        //    // keeps a separate list of items for each thread
+        //    // typically requires no synchronization, unless a thread tries to remove an item
+        //    // while the thread-local bag is empty (item stealing)
+        //    var bag = new ConcurrentBag<int>();
+        //    var tasks = new List<Task>();
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        var i1 = i;
+        //        tasks.Add(Task.Factory.StartNew(() =>
+        //        {
+        //            bag.Add(i1);
+        //            Console.WriteLine($"Thread{Task.CurrentId} has added {i1}");
+        //            int result;
+        //            if (bag.TryPeek(out result))
+        //            {
+        //                Console.WriteLine($"Thread{Task.CurrentId} has peeked the value {result}");
+        //            }
+        //        }));
+        //    }
+
+        //    Task.WaitAll(tasks.ToArray());
+
+        //    var isEmpty = bag.IsEmpty;
+        //    Console.WriteLine($"Bag is empty: {isEmpty}");
+
+
+        //    // take whatever's last
+        //    int last;
+        //    if (bag.TryTake(out last))
+        //    {
+        //        Console.WriteLine($"Last element is {last}");
+        //    }
+
+        //}
+    }
+}
+
