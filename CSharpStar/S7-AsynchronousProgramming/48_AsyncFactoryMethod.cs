915a9d502b30787ce16f1508c7f7cf7d1418d605
mingl <dreamingliu1101@gmail.com>
2022-10-28 16:03:44 +1100

    Add S7 files

---
+++ CSharpStar/S7-AsynchronousProgramming/48_AsyncFactoryMethod.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStar.S7_AsynchronousProgramming
+{
+    internal class AsyncFactoryMethod
+    {
+        public class Foo
+        {
+            public Foo()
+            {
+
+            }
+            public async Task<Foo> InitAsync()
+            {
+                await Task.Delay(1000);
+                return this;
+            }
+            // Factory method
+            public static Task<Foo> createAsync()
+            {
+                var result = new Foo();
+                return result.InitAsync();
+            }
+        }
+
+        public class Demo 
+        {
+            static async void Main(string[] args)
+            {
+                /*var foo = new Foo();
+                await foo.InitAsync();*/
+                Foo x = await Foo.createAsync();
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
